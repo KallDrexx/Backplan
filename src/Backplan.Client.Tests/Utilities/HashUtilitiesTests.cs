95e3458fa6f77593ddff511694f78a1718ec053e
KallDrexx <me@mshapiro.net>
2014-02-11 20:41:35 -0500

    Added method to compute the md5 hash of a stream

---
+++ src/Backplan.Client.Tests/Utilities/HashUtilitiesTests.cs
@@ -0,0 +1,47 @@
+﻿using Backplan.Client.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backplan.Client.Tests.Utilities
+{
+    [TestClass]
+    public class HashUtilitiesTests
+    {
+        [TestMethod]
+        public void Generates_Correct_Hash()
+        {
+            var testString = "Testing 12345";
+            var expectedHash = GenerateHash(testString);
+            string resultHash;
+
+            using (Stream stream = new MemoryStream())
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.Write(testString);
+                writer.Flush();
+                stream.Position = 0;
+
+                resultHash = HashUtilities.GenerateHash(stream);
+            }
+
+            Assert.AreEqual(expectedHash, resultHash, "Generated hash was incorrect");
+        }
+
+        private string GenerateHash(string input)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var encodedBytes = Encoding.UTF8.GetBytes(input);
+                var hashBytes = md5.ComputeHash(encodedBytes);
+                return BitConverter.ToString(hashBytes)
+                                   .Replace("-", "");
+            }
+        }
+    }
+}
---
+++ src/Backplan.Client/Utilities/HashUtilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backplan.Client.Utilities
+{
+    public static class HashUtilities
+    {
+        public static string GenerateHash(Stream stream)
+        {
+            var md5 = MD5.Create();
+            var hashBytes = md5.ComputeHash(stream);
+            return BitConverter.ToString(hashBytes)
+                               .Replace("-", "");
+        }
+    }
+}
