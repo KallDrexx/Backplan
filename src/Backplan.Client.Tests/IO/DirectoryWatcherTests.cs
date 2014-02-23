cfc616d0baa72d6ff2f1ab5328ded3b4addca84e
KallDrexx <me@mshapiro.net>
2014-02-23 14:31:24 -0500

    Added the DirectoryWatcher class and initial tests

---
+++ src/Backplan.Client.Tests/IO/DirectoryWatcherTests.cs
@@ -0,0 +1,57 @@
+﻿using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using Backplan.Client.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMoq;
+using Moq;
+
+namespace Backplan.Client.Tests.IO
+{
+    [TestClass]
+    public class DirectoryWatcherTests
+    {
+        private AutoMoqer _mocker;
+        private DirectoryWatcher _instance;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mocker = new AutoMoqer();
+            
+            // GetMock of the abstract class before create to prevent automoq bugs
+            _mocker.GetMock<FileSystemWatcherBase>();
+
+            _instance = _mocker.Create<DirectoryWatcher>();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Throws_Exception_If_Begin_Called_With_Null_Path()
+        {
+            _instance.Begin(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Throws_Exception_If_Begin_Called_With_Empty_Path()
+        {
+            _instance.Begin("");
+        }
+
+        [TestMethod]
+        public void Watcher_Gets_Path_Set_Same_As_Begin_Path_Parameter()
+        {
+            const string path = @"C:\test";
+            _mocker.GetMock<FileSystemWatcherBase>().SetupAllProperties();
+            _instance.Begin(path);
+
+            _mocker.GetMock<FileSystemWatcherBase>()
+                   .VerifySet(x => x.Path = path);
+        }
+    }
+}
---
+++ src/Backplan.Client/IO/DirectoryWatcher.cs
@@ -0,0 +1,31 @@
+﻿using Backplan.Client.Database;
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backplan.Client.IO
+{
+    /// <summary>
+    /// Watches a directory and notifies the tracked file store of changes
+    /// </summary>
+    public class DirectoryWatcher
+    {
+        private readonly FileSystemWatcherBase _fileSystemWatcher;
+
+        public DirectoryWatcher(FileSystemWatcherBase fileSystemWatcher)
+        {
+            _fileSystemWatcher = fileSystemWatcher;
+        }
+
+        public void Begin(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                throw new ArgumentException("FileSystemWatcher passed in without a valid path already set");
+
+            _fileSystemWatcher.Path = path;
+        }
+    }
+}
